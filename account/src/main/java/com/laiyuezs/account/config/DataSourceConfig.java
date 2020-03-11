6247ad601b8063c04a0f84408958b932c4ce57bf
liliang <liliang@laiyuezs.com>
2020-03-11 18:04:19 +0800

    seata代理HikariDataSource数据源

--- account/src/main/java/com/laiyuezs/account/config/DataSourceConfig.java
+++ account/src/main/java/com/laiyuezs/account/config/DataSourceConfig.java
@@ -1,7 +1,11 @@
 package com.laiyuezs.account.config;
 
 import com.alibaba.druid.pool.DruidDataSource;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 import io.seata.rm.datasource.DataSourceProxy;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,21 +19,40 @@ import javax.sql.DataSource;
 @Configuration
 public class DataSourceConfig {
 
-    @Bean
+/*    @Bean
     @ConfigurationProperties(prefix = "spring.datasource")
     public DruidDataSource druidDataSource() {
         return new DruidDataSource();
     }
 
-    /**
+    *//**
      * 需要将 DataSourceProxy 设置为主数据源，否则事务无法回滚
      *
      * @param druidDataSource The DruidDataSource
      * @return The default datasource
-     */
+     *//*
     @Primary
     @Bean("dataSource")
     public DataSource dataSource(DruidDataSource druidDataSource) {
         return new DataSourceProxy(druidDataSource);
+    }*/
+
+    @Autowired
+    private DataSourceProperties dataSourceProperties;
+
+    /**
+     * 构造datasource代理对象，替换原来的datasource
+     * @param
+     * @return
+     */
+    @Primary
+    @Bean("dataSource")
+    public DataSourceProxy dataSourceProxy() {
+        HikariConfig hikariConfig = new HikariConfig();
+        hikariConfig.setJdbcUrl(dataSourceProperties.getUrl());
+        hikariConfig.setUsername(dataSourceProperties.getUsername());
+        hikariConfig.setPassword(dataSourceProperties.getPassword());
+        HikariDataSource dataSource = new HikariDataSource(hikariConfig);
+        return new DataSourceProxy(dataSource);
     }
 }
